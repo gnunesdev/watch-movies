cdfa5b3adc9edd91077d66c4387698c2bdb59a93
Guilherme Nunes <gnunesinf@gmail.com>
2021-03-15 21:52:24 -0300

    fix: removendo interfaces desnecessrias

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,6 @@ import "./styles/global.scss";
 import "./styles/sidebar.scss";
 import "./styles/content.scss";
 
-interface GenreResponseProps {
-  id: number;
-  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
-  title: string;
-}
-interface MovieProps {
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
 export function App() {
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
